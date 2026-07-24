1711df8a5968fe74f55748ef049a9def1de0720b
신봉석 <stupic0713@gmail.com>
2026-07-24 17:59:11 +0900

    컬럼 너비 수정

--- src/app/(Auth)/partner-management/component/PartnerPage.tsx
+++ src/app/(Auth)/partner-management/component/PartnerPage.tsx
@@ -210,13 +210,13 @@ export default function PartnerPage() {
                         <col style={{width: '4%'}}/>
                         <col style={{width: '5%'}}/>
                         <col style={{width: '6%'}}/>
-                        <col style={{width: '10%'}}/>
-                        <col style={{width: '7%'}}/>
+                        <col style={{width: '13%'}}/>
+                        <col style={{width: '6%'}}/>
                         <col style={{width: '11%'}}/>
+                        <col style={{width: '6%'}}/>
                         <col style={{width: '6%'}}/>
                         <col style={{width: '7%'}}/>
                         <col style={{width: '7%'}}/>
-                        <col style={{width: '8%'}}/>
                         <col style={{width: '5%'}}/>
                         <col style={{width: '5%'}}/>
                         <col style={{width: '19%'}}/>
